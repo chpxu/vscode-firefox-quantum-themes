8893c2d36ffeebbc9a100a3cf6cd16aa8907803e
Progracientist <hi.progracientist@outlook.com>
2018-09-23 14:34:04 +0100

    UI Improvements, Testing Files

---
+++ test files/test.js
@@ -0,0 +1,23 @@
+// function toggleNav(currState) {
+//   if (currState === "open") {
+//     document.getElementById("dropdown").style.width = "0";
+//   } else if (currState === "close") {
+//     document.getElementById("dropdown").style.width = "250px";
+//   }
+// }
+function openNav() {
+  document.getElementById("mySidenav").style.height = "50vh";
+  document.getElementById("main").style.opacity = "0.9";
+}
+let t = ["xd", "xd"];
+t.forEach(element => console.log(t[element]));
+let x = 10;
+function closeNav() {
+  document.getElementById("mySidenav").style.height = "0";
+  document.getElementById("main").style.opacity = "1";
+}
+let z = {
+  lol: "xd",
+  sh: 10
+};
+z.lol;
